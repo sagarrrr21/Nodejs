d52e6d83a8dabbee508be525105f125d9c4a3bdc
SAGAR CHAUHAN <151016944+sagarrrr21@users.noreply.github.com>
2025-07-05 08:40:01 +0000

    module baisc commit

---
+++ Modules basics/basic.js
@@ -0,0 +1,7 @@
+const math = require("./math");
+
+console.log("Addition:", math.addFn(2, 3));
+console.log("Subtraction:", math.subFn(5, 2));
+
+// console.log("Addition : ", math.add(2, 3));
+// console.log("subtraction : ", math.sub(2, 3));
