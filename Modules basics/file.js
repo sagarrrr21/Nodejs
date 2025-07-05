7a29498fa38356464776b2bf7061815f283ddcd9
SAGAR CHAUHAN <151016944+sagarrrr21@users.noreply.github.com>
2025-07-05 12:38:38 +0000

    File Handling in nodejs

---
+++ Modules basics/file.js
@@ -0,0 +1,24 @@
+const fs = require("fs")
+// fs.writeFileSync('./test.txt' , 'hello my name is sagar')
+
+// fs.writeFile('./test.txt', 'hello my name is sagar asyc', (err) => { })
+
+// const result = fs.readFileSync('./contacts.txt', "utf-8")
+// console.log(result);
+
+// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+//     if (err)
+//         console.log("Error", err)
+//     else
+//         console.log(result)
+// })
+
+// fs.appendFileSync("./test.txt" , "hey there\n")
+// fs.appendFileSync("./test.txt" , `${Date.now()} Hey there\n`)
+
+// fs.cpSync("./test.txt" , "./copy.txt")
+
+
+// console.log(fs.statSync("./test.txt"))
+
+// fs.mkdirSync('my-doc')
